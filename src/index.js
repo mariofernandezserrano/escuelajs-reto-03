d3f36016094dc729cab88c615baae572e34eee84
Mario Fernandez Serrano <me.fernandezserrano@gmail.com>
2020-06-09 15:26:27 -0500

    I added ftechData file in Utils folder and solved challenge-03 Third Problem

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
+const fetchData = require('./utils/fetchData');
+
 const API = 'https://rickandmortyapi.com/api/character/';
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-
-const fetchData = (url_api) => {
-return new Promise((resolve, reject) => {
-  const xhttp = new XMLHttpRequest();
-  xhttp.open('GET', url_api, true);
-    xhttp.onreadystatechange = (() => {
-      if (xhttp.readyState === 4) {
-        (xhttp.status === 200)
-          ? resolve(JSON.parse(xhttp.responseText))
-          : reject(new Error('Error', url_api))
-          
-      }
-    });
-    xhttp.send();
-  });
-}
 
 fetchData(API) 
 .then(data => {
